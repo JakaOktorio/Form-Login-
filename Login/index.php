dfec1b890ac7b868b96e3ac181954e3c9cb33150
JakaOktorio <37045039+JakaOktorio@users.noreply.github.com>
2018-03-12 22:43:18 +0700

    Add files via upload

---
+++ Login/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tugas Topic 2 Jaka Oktorio 5150411185</title>
+        <link rel="icon" href="asset/images/Jaka Oktorio.jpg">
+        <link href="asset/css/csslogin.css" rel="stylesheet" type="text/css"/>
+        <style>
+            body{
+                background-image: url("asset/images/HD2-16549-sunset-at-the-mountain-lake-1920x1200-nature-wallpaper.jpg");
+                background-position: center;
+            }
+        </style>       
+    </head>
+    <body img src="asset/images/01-05-2013.jpg" alt="" >
+        <form id="frm01" name="frm01" method="POST" action="login.php">
+            <div class="vid-container">
+                <div class="inner-container">
+                    <div class="box">
+                        <center><img width="90" height="90" src="asset/images/person-1824144_960_720.png"></center>
+                        <h1>Menu Login</h1>
+                        <input class="form-control" placeholder="Username" type="text" required="yes" id="InputUsername" autofocus="yes" autocomplete="no"/>
+                        <input class="form-control" id="InputPassword" placeholder="Password" type="password" required="yes"/>
+                        <button>Login</button>
+                        <p>Not a member? <span>Sign Up</span></p>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
